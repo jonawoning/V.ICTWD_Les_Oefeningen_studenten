786a9bda2fd9d1a2bd20c9a50540da83202e7057
jonawoning <jonawoningyt@gmail.com>
2026-02-13 08:26:08 +0100

    Externalize styles, add JS, update HTML
    Move inline styles into a new opdr1.css, add interactivity in opdr1.js, and update opdracht_1.html to link the new files. CSS includes layout, hover, media (hide .item-2 under 768px) and print rules. JS provides clickItem alerts for items and a crashBrowser function (infinite alert) and HTML was updated to add onclick handlers and an extra clickable item.

---
+++ Sprint1_Les4_Reponsive/opdr1.js
@@ -0,0 +1,9 @@
+function clickItem(message) {
+    alert(message);
+}
+
+function crashBrowser() {
+    while (true) { 
+        alert('Je zit nu voor altijd vast aan een crash alert.');  
+    }
+}
